633306f9e10827522f667f2854eb210914e12bad
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-01 16:53:34 -0300

    Desativando Ector, Adicionando funcao para retornar usuarios

--- lib/api_example/repo.ex
+++ lib/api_example/repo.ex
@@ -1,3 +1,32 @@
 defmodule ApiExample.Repo do
-  use Ecto.Repo, otp_app: :api_example
+  def start_link do
+    {:ok, self}
+  end
+
+  # use Ecto.Repo, otp_app: :api_example
+  def all(ApiExample.User) do
+    [
+      %ApiExample.User{
+        id: 1,
+        name: "Joe",
+        email: "joe@example.com",
+        password: "topsecret",
+        stooge: "moe"
+      },
+      %ApiExample.User{
+        id: 2,
+        name: "Anne",
+        email: "anne@example.com",
+        password: "guessme",
+        stooge: "larry"
+      },
+      %ApiExample.User{
+        id: 3,
+        name: "Franklin",
+        email: "franklin@example.com",
+        password: "guessme",
+        stooge: "curly"
+      }
+    ]
+  end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,30 +2,7 @@ defmodule ApiExample.UserController do
   use ApiExample.Web, :controller
 
   def index(conn, _params) do
-    users = [
-      %ApiExample.User{
-        id: 1,
-        name: "Joe",
-        email: "joe@example.com",
-        password: "topsecret",
-        stooge: "moe"
-      },
-      %ApiExample.User{
-        id: 2,
-        name: "Anne",
-        email: "anne@example.com",
-        password: "guessme",
-        stooge: "larry"
-      },
-      %ApiExample.User{
-        id: 3,
-        name: "Franklin",
-        email: "franklin@example.com",
-        password: "guessme",
-        stooge: "curly"
-      }
-    ]
-
+    users = Repo.all(ApiExample.User)
     json(conn, users)
   end
 end
