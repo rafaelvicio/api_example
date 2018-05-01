094851a7f29166e250161d5f23deae383ac65fd1
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-01 16:42:52 -0300

    Criando Modelo de User

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,22 +1,31 @@
 defmodule ApiExample.UserController do
-    use ApiExample.Web, :controller
-  
-    def index(conn, _params) do
-        users = [
-            %{name: "Joe",
-              email: "joe@example.com",
-              password: "topsecret",
-              stooge: "moe"},
-            %{name: "Anne",
-              email: "anne@example.com",
-              password: "guessme",
-              stooge: "larry"},
-            %{name: "Franklin",
-              email: "franklin@example.com",
-              password: "guessme",
-              stooge: "curly"},
-          ]
-        json conn, users
-    end
+  use ApiExample.Web, :controller
+
+  def index(conn, _params) do
+    users = [
+      %ApiExample.User{
+        id: 1,
+        name: "Joe",
+        email: "joe@example.com",
+        password: "topsecret",
+        stooge: "moe"
+      },
+      %ApiExample.User{
+        id: 2,
+        name: "Anne",
+        email: "anne@example.com",
+        password: "guessme",
+        stooge: "larry"
+      },
+      %ApiExample.User{
+        id: 3,
+        name: "Franklin",
+        email: "franklin@example.com",
+        password: "guessme",
+        stooge: "curly"
+      }
+    ]
+
+    json(conn, users)
   end
-  
+end
